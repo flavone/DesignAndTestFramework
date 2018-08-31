0acce64444b9e00482a4a174d8c125fb913ac5be
Flavone.H <flavone@sina.com>
2018-08-31 18:14:47 +0800

    modify directory,add swagger parse utils

--- G.py
+++ G.py
@@ -6,6 +6,7 @@
 class ContentType:
     JSON = 'JSON'
     XML = 'XML'
+    XML_S = 'XML_SIMPLE'
     FORM_DATA = 'FORM_DATA'
     MULTI_DATA = 'MULTI_DATA'
 
@@ -25,9 +26,21 @@ class EncodeType:
     UTF8 = 'UTF8'
     GBK = 'GBK'
 
+
 AGE_RNG_CODE = [{'name': '年', 'code': 'Y'}, {'name': '月', 'code': 'M'}, {'name': '日', 'code': 'D'}]
 
 MAX_AGE = 65
 
 # TODO 添加完整区域码
 DISTRICT_CODE = ('110108', '120100', '429004', '430010', '130903', '510101', '622901')
+
+
+class RequestMethod:
+    POST = 'POST'
+    GET = 'GET'
+
+
+HEADERS = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+}
